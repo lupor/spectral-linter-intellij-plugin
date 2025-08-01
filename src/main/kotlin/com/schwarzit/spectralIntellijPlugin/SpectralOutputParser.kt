ee75ad2735b5506cde8cfa33df7a3d5bde5ae500
lupor <lupo.ribi@gmail.com>
2025-08-01 12:52:01 +0200

    Fixed issue where malformed results were returned by the tool, if there are no issues (#20)

--- src/main/kotlin/com/schwarzit/spectralIntellijPlugin/SpectralOutputParser.kt
+++ src/main/kotlin/com/schwarzit/spectralIntellijPlugin/SpectralOutputParser.kt
@@ -3,6 +3,7 @@ package com.schwarzit.spectralIntellijPlugin
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.Service
 import kotlinx.serialization.json.Json
+import org.springframework.util.StringUtils
 import java.text.ParseException
 
 @Service
@@ -14,6 +15,13 @@ class SpectralOutputParser {
 
     @Throws(ParseException::class)
     fun parse(input: String): List<SpectralIssue> {
+        if (!StringUtils.hasLength(input)) {
+            return emptyList()
+        }
+
+        if (input.length > 2 && input.startsWith("[]")) {
+            return emptyList()
+        }
         return Json.decodeFromString<List<SpectralIssue>>(input)
     }
 }
